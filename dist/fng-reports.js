dddade768601ce2328c712eb89a3e98d457f89e5
Mark Chapman <mark.chapman@gmail.com>
2020-11-14 13:44:12 +0000

    update deps

--- dist/fng-reports.js
+++ dist/fng-reports.js
@@ -1,4 +1,4 @@
-/*! forms-angular 2020-10-28 */
+/*! forms-angular 2020-11-14 */
 'use strict';
 
 formsAngular.controller('AnalysisCtrl', ['$rootScope', '$window', '$filter', '$scope', '$http', '$location', 'cssFrameworkService', 'routingService',
