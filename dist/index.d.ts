b03e2d0f28cb08fddd19b630d00a5c1a62a19c8e
Mark Chapman <mark.chapman@gmail.com>
2021-04-30 11:37:23 +0000

    Copy types file to dist

---
+++ dist/index.d.ts
@@ -0,0 +1,32 @@
+declare namespace fngReports {
+// See http://ui-grid.info/docs/#!/api/ui.grid.class:GridOptions.columnDef for details and full list of options
+    export interface IColumnDef {
+        name: string;
+        field: string;
+        displayName: string;
+        align?: string;
+        cellFilter?: string;
+        totalsRow?: string;
+        width?: number | string;
+        maxWidth?: number;
+        minWidth?: number;
+        enableFiltering?: boolean;
+        enableSorting?: boolean;
+        cellTemplate?: string;
+    }
+
+    export interface IMenuOption {
+        text: string;
+        url: string;
+    }
+
+    export interface IReportSchema {
+        pipeline: any;
+        title?: string;
+        drilldown?: string;
+        params?: any;
+        columnDefs?: IColumnDef[];
+        filter?: boolean;
+        menu?: IMenuOption[];
+    }
+}
