ba5d5fb282f2a0af146d2f1809033377bf178406
Mark Chapman <mark.chapman@gmail.com>
2021-07-05 15:31:27 +0000

    Build

--- dist/fng-reports.js
+++ dist/fng-reports.js
@@ -1,4 +1,4 @@
-/*! forms-angular 2021-06-16 */
+/*! forms-angular 2021-07-05 */
 'use strict';
 
 formsAngular.controller('AnalysisCtrl', ['$rootScope', '$window', '$filter', '$scope', '$http', '$location', 'cssFrameworkService', 'routingService',
