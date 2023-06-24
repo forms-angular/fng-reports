eafc677d1c616d31b0463340470d755b05e0f4bb
Ludeck <Ludeck1000@hotmail.com>
2023-06-24 11:28:33 +0000

    CssFrameworkService & RoutingService in the dist (presumably not important)

--- dist/fng-reports.js
+++ dist/fng-reports.js
@@ -1,15 +1,15 @@
 /*! forms-angular 2023-03-03 */
 'use strict';
 
-formsAngular.controller('AnalysisCtrl', ['$rootScope', '$window', '$filter', '$scope', '$http', '$location', 'cssFrameworkService', 'routingService',
-    function ($rootScope, $window, $filter, $scope, $http, $location, cssFrameworkService, routingService) {
+formsAngular.controller('AnalysisCtrl', ['$rootScope', '$window', '$filter', '$scope', '$http', '$location', 'CssFrameworkService', 'RoutingService',
+    function ($rootScope, $window, $filter, $scope, $http, $location, CssFrameworkService, RoutingService) {
         /*jshint newcap: false */
         var firstTime = true,
             pdfPlugIn = new ngGridPdfExportPlugin({inhibitButton: true}),
             csvPlugIn = new ngGridCsvExportPlugin({inhibitButton: true});
         /*jshint newcap: true */
 
-        angular.extend($scope, routingService.parsePathFunc()($location.$$path));
+        angular.extend($scope, RoutingService.parsePathFunc()($location.$$path));
 
         $scope.reportSchema = {
             columnDefs: []
@@ -38,7 +38,7 @@ formsAngular.controller('AnalysisCtrl', ['$rootScope', '$window', '$filter', '$s
                     var url = $scope.reportSchema.drilldown;
                     if (url) {
                         if (typeof url === 'string') {
-                            url = routingService.buildUrl(url.replace(/\|.+?\|/g, function (match) {
+                            url = RoutingService.buildUrl(url.replace(/\|.+?\|/g, function (match) {
                                 var param = match.slice(1, -1),
                                     isParamTest = /\((.+)\)/.exec(param);
                                 if (isParamTest) {
@@ -161,7 +161,7 @@ ${e.message}`);
                             type: thisPart.type || 'text',
                             required: true,
                             add: thisPart.add || undefined,
-                            size: thisPart.size || (cssFrameworkService.frameWork === 'bs3' ? 'large' : 'medium')
+                            size: thisPart.size || (CssFrameworkService.frameWork === 'bs3' ? 'large' : 'medium')
                         });
                         if (thisPart.type === 'select') {
                             // TODO: Remove when select and select2 is modified during the restructure
