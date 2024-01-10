d3b4df8fc4546f1014cec1868afd5dbd571dfae5
Mark Chapman <mark.chapman@gmail.com>
2024-01-10 12:58:55 +0000

    Revert ignore dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,45 @@
+declare namespace fngReports {
+// See http://ui-grid.info/docs/#!/api/ui.grid.class:GridOptions.columnDef for details and full list of options
+    export interface IColumnDef {
+        name: string;
+        field: string;
+        displayName?: string;
+        align?: string;
+        cellFilter?: string;
+        totalsRow?: string;
+        width?: number | string;
+        maxWidth?: number;
+        minWidth?: number;
+        enableFiltering?: boolean;
+        enableSorting?: boolean;
+        cellTemplate?: string;
+    }
+
+    export interface IMenuOption {
+        text: string;
+        url: string;
+    }
+
+    export interface IReportParam {
+        type: any;
+        value?: any;
+        noInput?: boolean;
+        size?: string;    // should be fng.FieldSizeString;
+        add?: string;
+        conversionExpression?: any;
+    }
+
+    export interface IReportParams {
+        [name: string]: IReportParam;
+    }
+
+    export interface IReportSchema {
+        pipeline: any;
+        title?: string;
+        drilldown?: string;
+        params?: IReportParams;
+        columnDefs?: IColumnDef[];
+        filter?: boolean;
+        menu?: IMenuOption[];
+    }
+}
